1efe62365f2a79491b1c682ec95db76082843bc1
= <=>
2022-04-05 15:47:17 +0300

    task1 implemented

---
+++ Hw3.Exercise1/ArithmeticApplication.cs
@@ -0,0 +1,46 @@
+﻿namespace Hw3.Exercise1
+{
+    public sealed class ArithmeticApplication
+    {
+        /// <summary>
+        /// Arithmetic application return codes.
+        /// </summary>
+        public enum ReturnCode
+        {
+            Success = 0,
+            InvalidArgs = -1,
+            Exception = -2
+        }
+
+        public ReturnCode Run(string[] args)
+        {
+            // TODO: Parse and validate the arguments and convert it to the numbers.
+            try
+            {
+                if (args is null)
+                    return ReturnCode.InvalidArgs;
+
+                var str = string.Join(",", args).Split(",", StringSplitOptions.RemoveEmptyEntries);
+
+                var parse = str.Select(s => int.TryParse(s, out var res)).ToList();
+
+                if (parse.Any(s => s == false) || parse.Count < 1)
+                    return ReturnCode.InvalidArgs;
+
+                var intList = Array.ConvertAll(str, int.Parse).ToList();
+
+                var result = ArithmeticProcessor.Calculate(intList);
+
+                Console.WriteLine(string.Join(" ", result));
+
+                return ReturnCode.Success;
+            }
+            catch (OverflowException)
+            {
+                return ReturnCode.Exception;
+            }
+
+        }
+
+    }
+}
---
+++ Hw3.Exercise1/ArithmeticProcessor.cs
@@ -0,0 +1,32 @@
+﻿namespace Hw3.Exercise1
+{
+    public static class ArithmeticProcessor
+    {
+        /// <summary>
+        /// Calculates numbers by algorithm.
+        /// </summary>
+        /// <param name="numbers">Collection of numbers.</param>
+        /// <returns>
+        /// Returns <c>IEnumerable</c> of sorted numbers. 
+        /// </returns>
+        /// <exception cref="ArgumentNullException">Sequence is null.</exception>
+
+        public static IEnumerable<int> Calculate(List<int> numbers)
+        {
+            if (numbers is null)
+                throw new ArgumentNullException(nameof(numbers));
+
+            var pairedIndex = numbers.Select((v, i) => new { Value = v, Index = i })
+                .Where(n => n.Index % 2 == 0)
+                .Select(n => checked(n.Value * 2)).ToList();
+
+            var unpairedIndex = numbers.Select((v, i) => new { Value = v, Index = i })
+                .Where(n => n.Index % 2 != 0)
+                .Select(n => checked(n.Value - 10)).ToList();
+
+            var result = pairedIndex.Concat(unpairedIndex).Distinct().OrderBy(result => result);
+            return result;
+
+        }
+    }
+}
